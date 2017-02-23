745da83a8ba2194c7a048c538ec7d3b9aaaf74bc
Kristina Simpson <sweet.kristas@gmail.com>
2017-02-23 14:32:59 +1300

    Added sol2 test.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "stdafx.h"
 
+#include "sol.hpp"
+#include <cassert>
+
+#pragma comment(lib, "lua51")
 
 int main()
 {
 	std::cerr << "Hello, World!\n";
+	sol::state lua;
+	int x = 0;
+	lua.set_function("beep", [&x]{ ++x; });
+	lua.script("beep()");
+	assert(x == 1);
 }
